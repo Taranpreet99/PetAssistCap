44abc0c9f7dbbdfa9d0e5d6db3891005fa83519f
Ryan <warnakur@sheridancollege.ca>
2020-10-27 11:17:17 -0400

    Updated the main storyboard. Updated the calendar page and training video page on storyboard  and updated the calendar view-controller to have basic event methods. Still working on it. - Ryan Warnakula

--- PetAssist/ViewControllers/CalendarViewController.swift
+++ PetAssist/ViewControllers/CalendarViewController.swift
@@ -11,10 +11,17 @@ import EventKit
 
 class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
+    
+    
+    //Connected to reminder textfield
+    @IBOutlet weak var eventText: UITextField!
     @IBOutlet var calendar: FSCalendar!
     let dateFormatter = DateFormatter()
     let date1 = Date()
     var dateText = ""
+    
+    //app delegate object ot use AppDelegate in this file
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,14 +86,87 @@ class CalendarViewController: UIViewController,FSCalendarDelegate, FSCalendarDat
         var rvc = segue.destination as! EventsViewController
         rvc.chosenDate = self.dateText
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Function to set event in the event app
+    @IBAction func setEvent(_ sender: AnyObject) {
+        
+        if appDelegate.eventStore == nil {
+            appDelegate.eventStore = EKEventStore()
+            
+            //Request for access to reminders app
+            appDelegate.eventStore?.requestAccess(
+                to: EKEntityType.event, completion: {(granted, error) in
+                    if !granted {
+                        print("Access to store not granted")
+                        print(error?.localizedDescription)
+                    } else {
+                        print("Access granted")
+                    }
+            })
+        }
+        
+        // If access has been granted and reminer has been saved
+        if (appDelegate.eventStore != nil) {
+            self.createEvents()
+            let alert = UIAlertController(title: "Event Saved", message: "Your event has been saved in the app. You will be notified.", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(ok)
+            present(alert, animated: true)
+        }
     }
-    */
 
+    
+    //Function to create a event
+    func createEvents() {
+        
+        let event = EKEvent(eventStore: appDelegate.eventStore!)
+        
+        //set the title of the reminder
+        event.title = eventText.text!
+        //set date and time
+        event.calendar =
+            appDelegate.eventStore!.defaultCalendarForNewReminders()
+        // Start of Event
+        event.startDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        // Entire Day
+        event.isAllDay = true
+        // End Date required
+        event.endDate = event.startDate
+        
+        do {
+            //Try to Save Event
+            try appDelegate.eventStore!.save(event, span: .thisEvent, commit: true)
+        } catch let error {
+            print("Event failed with error \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func removeEvent() {
+        var startDate=NSDate().addingTimeInterval(-60*60*24)
+        var endDate=NSDate().addingTimeInterval(60*60*24*3)
+        var predicate2 = appDelegate.eventStore!.predicateForEvents(withStart: startDate as Date, end: endDate as Date, calendars: nil)
+        
+        print("startDate:\(startDate) endDate:\(endDate)")
+        var eV = appDelegate.eventStore!.events(matching: predicate2) as [EKEvent]?
+        
+        if eV != nil {
+            for i in eV! {
+                print("Title  \(i.title)" )
+                print("stareDate: \(i.startDate)" )
+                print("endDate: \(i.endDate)" )
+                do{
+                    (try appDelegate.eventStore!.remove(i, span: EKSpan.thisEvent, commit: true))
+                }
+                catch let error {
+                }
+                
+            }
+        }
+    }
+        
 }
+    
+
--- PetAssist/ViewControllers/TrainingVideoViewController.swift
+++ PetAssist/ViewControllers/TrainingVideoViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Xcode User on 2020-10-22.
 //  Copyright © 2020 Taranpreet Singh Yu Zhang. All rights reserved.
 //
-
 import UIKit
 
 class TrainingVideoViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
@@ -23,23 +22,22 @@ class TrainingVideoViewController: UIViewController , UITableViewDelegate, UITab
         return cell
     }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.playerView.load(withVideoId: "M7lc1UVf-VE")
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
